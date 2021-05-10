dad1805e55ed96d1f4437d243e865636f3e1580a
letmeNo1 <fkzzzz@qq.com>
2021-05-10 16:28:57 +0800

    11
    11

--- main.cc
+++ main.cc
@@ -4,58 +4,70 @@
 
 #define MAX_DISPLAYS 16
 // 定义 Add 函数
-Napi::Value getWindowRect(const Napi::CallbackInfo& info) {
+Napi::Value getDesktopCapture(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
 
   // 接收参数
-    double arg0 = info[0].As<Napi::Number>().DoubleValue();
+    std::string ags0 = info[0].As<Napi::String>();
+    double ags1 = info[1].As<Napi::Number>().DoubleValue();
+    double x = info[2].As<Napi::Number>().DoubleValue();
+    double y = info[3].As<Napi::Number>().DoubleValue();
+    double w = info[4].As<Napi::Number>().DoubleValue();
+    double h = info[5].As<Napi::Number>().DoubleValue();
+    std::string screenshotName = info[6].As<Napi::String>();
 
-    std::string baseImageOutput = "file:///Users/hank.huang/Desktop";
+
+
+    std::string baseImageOutput = "file:///"+ags0;
     std::string pathSeparator = "/";
-    std::string baseImageName = "image-";
     std::string imageExtension = ".png";
-
     CGDisplayErr      dErr;
     CGDisplayCount    displayCount, i;
-    CGDirectDisplayID onlineDisplays[ MAX_DISPLAYS ];
+    CGDirectDisplayID mainDisplay;
+    CGDisplayCount    maxDisplays = MAX_DISPLAYS;
+    CGDirectDisplayID onlineDisplays[MAX_DISPLAYS];
+    CGImageRef image;
 
-    dErr = CGGetOnlineDisplayList(2, onlineDisplays, &displayCount);
+    dErr = CGGetOnlineDisplayList(maxDisplays, onlineDisplays, &displayCount);
     if (dErr != kCGErrorSuccess) {
             fprintf(stderr, "CGGetOnlineDisplayList: error %d.\n", dErr);
             exit(1);
     }
-    for (i = 0; i < displayCount; i++) {
-            std::string imagePath = baseImageOutput + pathSeparator + baseImageName + std::to_string(i) + imageExtension;
-            const char *charPath = imagePath.c_str();
 
-            CFStringRef imageOutputPath = CFStringCreateWithCString(kCFAllocatorDefault, charPath, kCFURLPOSIXPathStyle);
-
-            CGDirectDisplayID dID = onlineDisplays[i];
-            CGImageRef image = CGDisplayCreateImage(dID);
-            CFURLRef url = CFURLCreateWithString(kCFAllocatorDefault, imageOutputPath, NULL);
-            CGImageDestinationRef destination = CGImageDestinationCreateWithURL(url, kUTTypePNG, 1, NULL);
-            if (!destination) {
-            std::cout<< "The destination does not exist: " << imagePath << std::endl;
-            CGImageRelease(image);
-        }
-            CGImageDestinationAddImage(destination, image, NULL);
-            if (!CGImageDestinationFinalize(destination)) {
-            std::cout << "Failed to write image to the path" << std::endl;;
-            CFRelease(destination);
-            CGImageRelease(image);
-        }
-            CFRelease(destination);
-            CGImageRelease(image);    
-            // UIImage* image = [UIImage imageWithCGImage: cgRef];
-            printf("%-16p", dID);
-           
+    std::string imagePath = baseImageOutput + pathSeparator + screenshotName + imageExtension;
+    const char *charPath = imagePath.c_str();
+    CFStringRef imageOutputPath = CFStringCreateWithCString(kCFAllocatorDefault, charPath, kCFURLPOSIXPathStyle);
+    CGDirectDisplayID dID = ags1;
+    
+    image = CGDisplayCreateImageForRect(dID,CGRectMake(x,y,w,h));
+ 
+   
+    CFURLRef url = CFURLCreateWithString(kCFAllocatorDefault, imageOutputPath, NULL);
+    CGImageDestinationRef destination = CGImageDestinationCreateWithURL(url, kUTTypePNG, 1, NULL);
+    if (!destination) {
+      std::cout<< "The destination does not exist: " << imagePath << std::endl;
+      CGImageRelease(image);
+      }
+    CGImageDestinationAddImage(destination, image, NULL);
+    if (!CGImageDestinationFinalize(destination)) {
+      std::cout << x << std::endl;
+      std::cout << y << std::endl;
+      std::cout << w << std::endl;
+      std::cout << h << std::endl;
+      std::cout << "Failed to write image to the path" << std::endl;
+      CFRelease(destination);
+      CGImageRelease(image);
     }
-}
+    CFRelease(destination);
+    CGImageRelease(image);    
 
+    Napi::String imagePath2 = Napi::String::New(env, imagePath);
+    return imagePath2;
+}
 // 入口函数，用于注册我们的函数、对象等等
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   // 将一个名为 add 的函数挂载到 exports 上
-  exports.Set(Napi::String::New(env, "getWindowRect"), Napi::Function::New(env, getWindowRect));
+  exports.Set(Napi::String::New(env, "getDesktopCapture"), Napi::Function::New(env, getDesktopCapture));
   return exports;
 }
 
